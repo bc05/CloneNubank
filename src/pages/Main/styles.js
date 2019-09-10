814a04b10da1b2d6a4593aece52603f1225f43c9
Odilon Garcez <odilon.garcez.moraes@gmail.com>
2019-09-10 16:23:19 -0300

    feat:  espaçamento do topo da tela para dispositivos com dot bar

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components/native';
 
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+
 export const Container = styled.View`
   flex: 1;
   background: #8A05BE
+  padding-top: ${getStatusBarHeight()}px;
 `;
