c0da0098332b29632db5c98e1ae9f5398b28b446
Odilon Garcez <odilon.garcez.moraes@gmail.com>
2019-09-10 16:24:00 -0300

    feat: cabeçalho da página inicial

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Container, Top, Logo, Title } from "./styles";
+
+import Icon from "react-native-vector-icons/MaterialIcons";
+
+import logo from "~/assets/Nubank_Logo.png";
+
+export default function Header() {
+    return (
+        <Container>
+            <Top>
+                <Logo source={logo} />
+                <Title>Odilon Garcez</Title>
+            </Top>
+            <Icon name="keyboard-arrow-down" sie="20" color="#FFF" />
+        </Container>
+    );
+}
