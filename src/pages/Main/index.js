bfa1544d87946d7a96ae3bd8e9e7182aa62767d5
Odilon Garcez <odilon.garcez.moraes@gmail.com>
2019-09-10 20:50:46 -0300

    feat: adicionado card na tela inicial do app

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import { Container, Content } from "./styles";
+
 import Header from "~/components/Header";
-import { Container } from "./styles";
-import Tabs from '~/components/Tabs';
+import Tabs from "~/components/Tabs";
+import Card from "~/components/Card";
 
 export default function Main() {
   return (
       <Container>
         <Header />
+
+        <Content>
+          <Card />
+        </Content>
+
         <Tabs />
       </Container>
     );
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -6,4 +6,12 @@ export const Container = styled.View`
   flex: 1;
   background: #8A05BE
   padding-top: ${getStatusBarHeight()}px;
+  justify-content: center;
+  padding-bottom: 15px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  max-height: 400px;
+  z-index: 5;
 `;
